301bd2c5869950f1f6212f63e10f3b6aaec3fb89
j-ellis13 <j.ellis@digitalfoundry.com>
2017-05-23 08:51:08 -0700

    Refactor common module import

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -1,5 +1,17 @@
-import Spacer from './Spacer'
-export {
-  Spacer
-}
+import _ from 'underscore'
+
+const context = require.context(__dirname, true, /\/.+\/index\.js$/)
+const data = _.chain(context.keys())
+  .map((key) => {
+    const fileData = context(key)
+    const branchName = key.match(/\.\/(.+)\/index.js/i)[1]
+    return [
+      branchName,
+      fileData.default
+    ]
+  })
+  .object()
+  .value()
+
+module.exports = data
 
